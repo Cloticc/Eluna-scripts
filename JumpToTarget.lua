4ecbc44bf59d5f60b8424f705993f2be31facd04
Clotic <isidorssona@gmail.com>
2020-07-07 17:27:06 +0200

    Update JumpToTarget.lua
    Added locals

--- JumpToTarget.lua
+++ JumpToTarget.lua
@@ -1,19 +1,18 @@
 local MSG_TJ = "#tj"
 
 local function TargetJump(event, player, msg, Type, lang)
-    selectTarget = player:GetSelection()
-    if (not selectTarget) then
-        selectTarget = player
+    local selectTarget = player:GetSelection()-- Make it so you select target
+    if (not selectTarget) then -- Make it so you select urself
+        selectTarget = player -- Make it so you select urself
     end
-
-    gmRank = player:GetGMRank()
+    
+    local gmRank = player:GetGMRank()
     if (gmRank >= 3) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
-        msg = msg:lower()
+        local msg = msg:lower()
         if (msg == MSG_TJ) then
             x = selectTarget:GetX()
             y = selectTarget:GetY()
             z = selectTarget:GetZ()
-            -- o = :GetO()
             player:MoveJump(x, y, z, 20, 25, 0)
         end
     end
