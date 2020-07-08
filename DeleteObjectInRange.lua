8469d0559bc1faf9bb80fa0c70fb4be3c1ed17f8
Clotic <isidorssona@gmail.com>
2020-07-08 07:15:11 +0200

    Update DeleteObjectInRange.lua
    more changes added so you can do #obc to check info without delete it.

--- DeleteObjectInRange.lua
+++ DeleteObjectInRange.lua
@@ -1,10 +1,13 @@
-local MSG_GD = "#gd" -- What messege in chat box
+-- dg deletes object and you get info
+-- dc check info will not delete the item
+local MSG_GD = "#obd" -- What messege in chat box
+local MSG_GC = "#obc" -- What messege in chat box
 local RANGE = 10 -- What range you want the delete to detect in.
 
 local function DeleteObjectNearby(event, player, msg, Type, lang)
     local gmRank = player:GetGMRank()
     if (gmRank >= 3) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
-        local msg = msg:lower()
+        msg = msg:lower()
         if (msg == MSG_GD) then
             local a = player:GetNearestGameObject(RANGE)
             local b = a:GetEntry()
@@ -12,10 +15,24 @@ local function DeleteObjectNearby(event, player, msg, Type, lang)
             local d = a:GetName()
             local e = a:GetGUIDLow()
             a:RemoveFromWorld(true)
-            player:SendBroadcastMessage(
-            ("|cFFffffff Name: |r " .. d .. " |cFFffffff Guid Low: |r " .. e .. " |cFFffffff Entry Deleted: |r  " .. b .. " |cFFffffff Display ID: |r " .. c .. "!")
-        )
+            player:SendBroadcastMessage(("|cFFffffff Name: |r " .. d .. " |cFFffffff Guid Low: |r " .. e .. " |cFFffffff Entry Deleted: |r  " .. b .. " |cFFffffff Display ID: |r " .. c .. ""))
+        end
+        
+        
+        
+        if (msg == MSG_GC) then
+            local a = player:GetNearestGameObject(RANGE)
+            local b = a:GetEntry()
+            local c = a:GetDisplayId()
+            local d = a:GetName()
+            local e = a:GetGUIDLow()
+            player:SendBroadcastMessage(("|cFFffffff Name: |r " .. d .. " |cFFffffff Guid Low: |r " .. e .. " |cFFffffff Entry Deleted: |r  " .. b .. " |cFFffffff Display ID: |r " .. c .. ""))
+        
         end
     end
 end
+
+
+
+
 RegisterPlayerEvent(18, DeleteObjectNearby)
