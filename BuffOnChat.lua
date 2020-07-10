3c953fb3e1dc7256d71e606ea2bf54898f88b24d
Clotic <isidorssona@gmail.com>
2020-07-10 20:35:42 +0200

    Update added folders

---
+++ BuffOnChat.lua
@@ -0,0 +1,61 @@
+--Probably want to change Buffs you want active. All the non targeted once are normal class buffs. 
+local MSG_BUFF = "#buff"
+local Cost = 1000000 --- How much buff msg cost to use. 10gold atm
+local buffID = {
+    26393, --Eluns blessing 10% Stats
+    23735, -- Sayge's Dark Fortune of Strength
+    23737, -- Sayge's Dark Fortune of Stamina
+    23738, -- Sayge's Dark Fortune of Stamina
+    23769, -- Sayge's Dark Fortune of Resistance
+    23766, -- Sayge's Dark Fortune of Intelligence
+    23768, -- Sayge's Dark Fortune of Damage
+    23767, -- Sayge's Dark Fortune of Armor
+    23736, -- Sayge's Dark Fortune of Agility
+    48074,
+    48170,
+    43223,
+    32728, --Arena prep free spell casting
+    36880,
+    467,
+    48469,
+    48162,
+    21564,
+    26035,
+    48469,
+    48073,
+    16609,
+    36880,
+    15366,
+    43223,
+    -- 65077, -- tower of frost 40% health
+    -- 65075, -- tower of fire 40% health 50% fire damage
+    48074,
+    38734, -- Master Ranged Buff
+    35912, -- Master Magic Buff
+    35874 -- Master Melee Buff
+}
+
+local function BuffOnMsg(event, player, msg, type, language)
+    local gmRank = player:GetGMRank()
+    if (gmRank >= 3) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
+        selectTarget = player:GetSelection()
+        if (not selectTarget) then
+            selectTarget = player
+        end
+    end
+    
+    msg = msg:lower()
+    if (msg == MSG_BUFF) then
+        if (player:GetCoinage() >= Cost) then
+            player:ModifyMoney(-Cost)
+            for k, v in pairs(buffID) do
+                selectTarget:AddAura(v, selectTarget)
+            end
+            player:SendBroadcastMessage("|cFDFEFEbuffed, Enjoy!|r")
+        end
+        
+        return false
+    end
+end
+
+RegisterPlayerEvent(18, BuffOnMsg)
---
+++ NpcBuffOnSight.lua
@@ -0,0 +1,58 @@
+
+--Probably want to change Buffs you want active. All the non targeted once are normal class buffs. 
+local NPC_ID = xXx -- Npcs ID
+
+--Remove or add buffs you want the npc to buff. BUFF_CHECK is buff that npc will check if u got buffs.
+local BUFF_CHECK = 467
+
+local BUFF_IDS = {
+    23735, -- Sayge's Dark Fortune of Strength
+    23737, -- Sayge's Dark Fortune of Stamina
+    23738, -- Sayge's Dark Fortune of Stamina
+    23769, -- Sayge's Dark Fortune of Resistance
+    23766, -- Sayge's Dark Fortune of Intelligence
+    23768, -- Sayge's Dark Fortune of Damage
+    23767, -- Sayge's Dark Fortune of Armor
+    23736, -- Sayge's Dark Fortune of Agility
+    26393, --Eluns blessing 10% Stats
+    48074,
+    48170,
+    43223,
+    36880,
+    467,
+    48469,
+    48162,
+    32728, --Arena prep free spell casting
+    21564,
+    26035,
+    48469,
+    48073,
+    16609,
+    36880,
+    15366,
+    43223,
+    -- 65077, -- tower of frost 40% health
+    -- 65075, -- tower of fire 40% health 50% fire damage
+    38734, -- Master Ranged Buff
+    35912, -- Master Magic Buff
+    35874, -- Master Melee Buff
+    48074
+}
+
+local function BuffOnSight(event, creature, player)
+    if creature:IsInRange(player, 1, 15) then
+       local playerGUID = player:GetGUIDLow()
+        if (playerGUID > 0) then
+            local player = GetPlayerByGUID(playerGUID)
+            if (player ~= nil) then
+                if not player:HasAura(BUFF_CHECK) then
+                    for k, v in pairs(BUFF_IDS) do
+                        player:AddAura(v, player)
+                    end
+                    creature:SendChatMessageToPlayer(8, 0, "You been buffed enjoy.", player)
+                end
+            end
+        end
+    end
+end
+RegisterCreatureEvent(NPC_ID, 27, BuffOnSight)
