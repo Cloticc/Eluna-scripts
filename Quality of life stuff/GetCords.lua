61eca97ff7c9871c2dc7f0d20da1776b152967e9
Clotic <isidorssona@gmail.com>
2020-07-17 16:02:45 +0200

    Update
    Removed and old file and edit Cords

--- Quality of life stuff/GetCords.lua
+++ Quality of life stuff/GetCords.lua
@@ -4,14 +4,20 @@ local MSG_CO = "#xyz" --#command for triggering
 local function RandomPenis(event, player, msg, Type, lang)
     local msg = msg:lower()
     if (msg == MSG_CO) then
+        
         local x = player:GetX()
         local y = player:GetY()
         local z = player:GetZ()
         local o = player:GetO()
-        player:SendBroadcastMessage(("|cffffffff Test X |r " .. x .. ""))
-        player:SendBroadcastMessage(("|cffffffff Test Y |r " .. y .. ""))
-        player:SendBroadcastMessage(("|cffffffff Test Z |r " .. z .. ""))
-        player:SendBroadcastMessage(("|cffffffff Test O |r " .. o .. ""))
+    
+
+
+        player:SendBroadcastMessage(("|cffffffff X |r " .. x .. ""))
+        player:SendBroadcastMessage(("|cffffffff Y |r " .. y .. ""))
+        player:SendBroadcastMessage(("|cffffffff Z |r " .. z .. ""))
+        player:SendBroadcastMessage(("|cffffffff O |r " .. o .. ""))
+
+
         return false
     end
 
