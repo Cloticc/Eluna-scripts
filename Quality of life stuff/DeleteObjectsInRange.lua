d0c5b0262a255f525358f11ad824a608f50f19e8
Clotic <isidorssona@gmail.com>
2020-07-23 19:51:03 +0200

    Update DeleteObjectsInRange.lua
    Cleaned up code.

--- Quality of life stuff/DeleteObjectsInRange.lua
+++ Quality of life stuff/DeleteObjectsInRange.lua
@@ -1,38 +1,38 @@
 -- obd deletes object and you get info
 -- obc check info will not delete the item
-local MSG_OBD = "#obd" -- What messege in chat box
-local MSG_OBC = "#obc" -- What messege in chat box
+local MSG_OBD = "#obd" -- What Message in chat box
+local MSG_OBC = "#obc" -- What Message in chat box
 
 local RANGE = 10 -- What range you want the delete to detect in.
 
+-------------------------------------------------------------------------
 local function DeleteObjectNearby(event, player, msg, _, lang)
     local gmRank = player:GetGMRank()
     if (gmRank >= 3) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
+        if player:GetNearestGameObject(RANGE) then
+            player:SendBroadcastMessage("nothing here")
+        end
+        
+        local a = player:GetNearestGameObject(RANGE)
+        local b = a:GetEntry()
+        local c = a:GetDisplayId()
+        local d = a:GetName()
+        
         if (msg:find(MSG_OBD)) then
-            local a = player:GetNearestGameObject(RANGE)
-            local b = a:GetEntry()
-            local c = a:GetDisplayId()
-            local d = a:GetName()
             a:RemoveFromWorld(true)
+            
             player:SendBroadcastMessage(
                 "|cFFffffff Name: |r " ..
-                    d .. " |cFFffffff Entry Deleted: |r  " .. b .. " |cFFffffff Display ID: |r " .. c .. ""
+                d .. " |cFFffffff Entry Deleted: |r  " .. b .. " |cFFffffff Display ID: |r " .. c .. ""
             )
             return false
-        end
-
-        if  (msg:find(MSG_OBC)) then
-            local a = player:GetNearestGameObject(RANGE)
-            local b = a:GetEntry()
-            local c = a:GetDisplayId()
-            local d = a:GetName()
+        elseif (msg:find(MSG_OBC)) then
             player:SendBroadcastMessage(
                 "|cFFffffff Name: |r " .. d .. " |cFFffffff Entry:|r  " .. b .. " |cFFffffff Display ID: |r " .. c .. ""
-        )
+            )
             return false
         end
     end
 end
-
 
 RegisterPlayerEvent(18, DeleteObjectNearby)
