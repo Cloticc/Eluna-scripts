5589b79f67be70ca9482b20b20dc5f52e797ef30
Clotic <isidorssona@gmail.com>
2020-07-28 16:12:20 +0200

    Update Gender.lua
    Fixed Combat issue

--- Quality of life stuff/Gender.lua
+++ Quality of life stuff/Gender.lua
@@ -2,12 +2,15 @@ local MSG_GEN = "#gender"
 local Cost = 1000000 --This is in copper so the value that is now 100g so 1000000c
 
 local function SwitchGender(event, player, msg, Type, lang)
-    if player:IsInCombat() then
-        player:SendBroadcastMessage("You are in combat!")
-    else
+    
+    
         local gmRank = player:GetGMRank()
         if (gmRank >= 0) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
             if (msg:find(MSG_GEN)) then
+                if player:IsInCombat() then
+                    player:SendBroadcastMessage("You are in combat!")
+                    return false
+                else
                 if player:GetCoinage() >= Cost then
                 else
                     player:SendAreaTriggerMessage("You need 100 gold")
@@ -15,7 +18,7 @@ local function SwitchGender(event, player, msg, Type, lang)
                 end
                 player:SetCoinage(player:GetCoinage() - Cost)
                 player:SendAreaTriggerMessage("You feel a change.")
-                player:SendBroadcastMessage("100 gold been taken")
+                player:SendAreaTriggerMessage("100 gold been taken")
 
                 if player:GetGender() == 0 then
                     player:SetGender(1)
