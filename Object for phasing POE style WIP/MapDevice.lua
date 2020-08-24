95b3651afbcc19ffed52e3368887744a16f884b0
Clotic <isidorssona@gmail.com>
2020-08-24 14:59:59 +0200

    Update MapDevice.lua
    Been testing making Path of exile portal phasing system.

--- Object for phasing POE style WIP/MapDevice.lua
+++ Object for phasing POE style WIP/MapDevice.lua
@@ -3,11 +3,19 @@ local objectid = 244607 -- object id for device
 local portal = 244606 -- object for portal ID
 local Cost = 1000000 -- Cost to Open portal
 local Phase = 10 -- sets phase to go to
+local Monsters = 43283
 
 local function MapDevice(event, player, object)
     player:GossipClearMenu()
     player:GossipMenuAddItem(0, "Activate", 0, 1, false, "Are you sure", 1000000)
-    player:GossipMenuAddItem(0, "Deactivate", 0, 2, false, "Are you sure")
+    player:GossipMenuAddItem(
+        0,
+        "Deactivate",
+        0,
+        2,
+        false,
+        "Are you sure. Clicking this will despawn the Device & Portal"
+    )
     player:GossipSendMenu(99, object)
     return false
 end
@@ -16,6 +24,10 @@ local function MapDeviceOnSelect(event, player, object, sender, intid, code, men
     if (intid == 1) then
         if (player:GetCoinage() >= Cost) then
             player:ModifyMoney(-Cost)
+
+            local map = object:GetMapId()
+            local instanceid = object:GetInstanceId()
+            local o = object:GetO()
             local x = object:GetX()
             local y = object:GetY()
 
@@ -27,19 +39,9 @@ local function MapDeviceOnSelect(event, player, object, sender, intid, code, men
                 --   print( pt )
                 local x1, y1, z1 = object:GetRelativePoint(4, pt)
 
-                PerformIngameSpawn(
-                    2,
-                    portal,
-                    object:GetMapId(),
-                    object:GetInstanceId(),
-                    x1,
-                    y1,
-                    z1,
-                    object:GetO(),
-                    false,
-                    2,
-                    1
-                )
+                PerformIngameSpawn(2, portal, map, instanceid, x1, y1, z1, o, false, 2, 1)
+
+
             end
             player:GossipComplete()
         end
@@ -74,13 +76,24 @@ RegisterGameObjectGossipEvent(objectid, 1, MapDevice)
 RegisterGameObjectGossipEvent(objectid, 2, MapDeviceOnSelect)
 RegisterGameObjectGossipEvent(portal, 1, PortalOnHello)
 RegisterGameObjectGossipEvent(portal, 2, PortalOnSelect)
-
 -- RegisterCreatureGossipEvent(npcid, 1, MapDevice)
 -- RegisterCreatureGossipEvent(npcid, 2, MapDeviceActivate)
-
 -- 1 * math.pi / 180
 --  65 * math.pi / 180
 -- 120 * math.pi / 180
 -- 185 * math.pi / 180
 -- 245 * math.pi / 180
 -- 290 * math.pi / 180
+
+
+
+
+-- PerformIngameSpawn( spawnType, entry, mapId, instanceId, x, y, z, o, save, durorresptime, phase )
+
+-- for p = 1, 25 do
+--     w = p * math.pi / 180
+--     pt = r + r * math.cos(w) and y + r * math.sin(w)
+--     --   print( pt )
+--     local x2, y2, z2 = object:GetRelativePoint(6, pt)
+-- end
+-- PerformIngameSpawn(2, Monsters, map, instanceid, x2, y2, z2, o, false, 2, Phase)
