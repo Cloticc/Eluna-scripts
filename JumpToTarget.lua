3ec94bc7ebf1930fe4149a24ad2160c9a3cb07f3
Clotic <isidorssona@gmail.com>
2020-09-01 21:18:40 +0200

    Change how to trigger word

--- JumpToTarget.lua
+++ JumpToTarget.lua
@@ -3,13 +3,12 @@ local MSG_TJ = "#tj"
 local function TargetJump(event, player, msg, Type, lang)
     local selectTarget = player:GetSelection()-- Make it so you select target
     if (not selectTarget) then -- Make it so you select urself
-        selectTarget = player -- Make it so you select urself
+         selectTarget = player -- Make it so you select urself
     end
     
     local gmRank = player:GetGMRank()
     if (gmRank >= 3) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
-        local msg = msg:lower()
-        if (msg == MSG_TJ) then
+if (msg:find(MSG_TJ)) then
             x = selectTarget:GetX()
             y = selectTarget:GetY()
             z = selectTarget:GetZ()
--- Quality of life stuff/Advanceallskills.lua
+++ Quality of life stuff/Advanceallskills.lua
@@ -4,19 +4,14 @@ function AdvanceSkillsToMax(event, player, msg, type, language)
     
     gmRank = player:GetGMRank()
     if (gmRank >= 3) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
-        selectTarget = player:GetSelection()
-        if (not selectTarget) then
-            selectTarget = player
+        
+            if (msg:find(MSG_ADV)) then
+            player:AdvanceAllSkills(999)
+            player:AdvanceSkillsToMax()
+            player:SendBroadcastMessage("You skills been maxed")
+            return false
         end
     end
-    msg = msg:lower()
-    if (msg == MSG_ADV) then
-        selectTarget:AdvanceAllSkills(999)
-        selectTarget:AdvanceSkillsToMax()
-        selectTarget:SendBroadcastMessage("You skills been maxed")
-        return false
-    end
-    
 end
 
 
