5ccf76921ecb39b5663ef53f15384370742fa462
Clotic <isidorssona@gmail.com>
2020-11-01 19:55:33 +0100

    Spaces

--- Quality of life stuff/Gender.lua
+++ Quality of life stuff/Gender.lua
@@ -4,10 +4,8 @@ local MSG_GEN = "#gender"
 local Cost = 0 --This is in copper so the value that is now 100g so 1000000c
 local Gold = 0 -- Gold value for visual on screen to showup. Can probably improve this later
 
-function SwitchGender(event, player, msg, Type, lang)
+local function SwitchGender(event, player, msg, Type, lang)
     if (msg:find(MSG_GEN)) then
-
-
         local gmRank = player:GetGMRank()
         if (gmRank <= 2) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
             player:SendAreaTriggerMessage("You Dont have access to this.")
@@ -19,7 +17,7 @@ function SwitchGender(event, player, msg, Type, lang)
             player:SendAreaTriggerMessage("You require" .. Gold .. " Gold")
             return false
         end
-        
+
         player:SetCoinage(player:GetCoinage() - Cost)
         player:SendAreaTriggerMessage(Gold .. " Gold Been taken")
 
@@ -32,8 +30,6 @@ function SwitchGender(event, player, msg, Type, lang)
             player:SetGender(0)
             return false
         end
-        
     end
 end
-
 RegisterPlayerEvent(18, SwitchGender)
