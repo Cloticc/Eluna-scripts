29821f87c44c63081a761d31a06a4ea289a55f6e
Clotic <isidorssona@gmail.com>
2020-11-01 19:56:25 +0100

    Change msg name

--- Quality of life stuff/Advanceallskills.lua
+++ Quality of life stuff/Advanceallskills.lua
@@ -1,10 +1,10 @@
-local MSG_ADV = "#ss"
+local MSG_ADV = "#adv"
 
 function AdvanceSkillsToMax(event, player, msg, type, language)
-    
+
     gmRank = player:GetGMRank()
     if (gmRank >= 3) then -- change number (0-3) 0 - to all  1,2,3 GM with rank
-        
+
             if (msg:find(MSG_ADV)) then
             player:AdvanceAllSkills(999)
             player:AdvanceSkillsToMax()
