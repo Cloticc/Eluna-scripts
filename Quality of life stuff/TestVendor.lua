334feba940eabb58ba4379dd3592b545901bd575
Clotic <isidorssona@gmail.com>
2020-11-03 20:28:53 +0100

    Update TestVendor.lua

--- Quality of life stuff/TestVendor.lua
+++ Quality of life stuff/TestVendor.lua
@@ -1,46 +1,51 @@
+--[[ made for testing  ]]
+
 local npcid = 43284
 
 -- local T = {
 -- [0] = {}}
-local DatabaseCache = {}
-LoadDatabase(player)
-    local ExampleQuery = CharDBQuery("SELECT guid, spell FROM character_spell")
-    if (ExampleQuery) then
+
+
+
+local function LoadDB(player)
+    local guid = player:GetGUIDLow()
+    local Q = CharDBQuery("SELECT spell FROM character_spell where guid = "..guid)
+    if (Q) then
         repeat
-            DatabaseCache[ExampleQuery:GetUInt32(0)] = {
-                Spell = ExampleQuery:GetUInt32(1)
-            }
+            local spell = Q:GetUInt32(0)
             player:RemoveSpell(spell, false, true)
-        until not ExampleQuery:NextRow()
+        until not Q:NextRow()
     end
 end
--- LoadDataBase()
 
--- function LoadDB(player)
---     -- DD = {}
---     local Q = CharDBQuery("SELECT spell FROM character_spell")
---     if (Q) then
---         repeat
---             local spell = Q:GetUInt32(0)
---             player:RemoveSpell(spell, false, true)
---         until -- DD[guid] = spell
---         not Q:NextRow()
---     end
--- end
--- LoadDB()
 
--- local guid = player:GetGUIDLow()
--- local Q = CharDBQuery("SELECT spell FROM character_spell WHERE guid =" .. guid)
--- if (Q) then
---     repeat
---         local spell = Q:GetUInt32(0)
---         player:RemoveSpell(spell, false, true)
---     until not Q:NextRow()
--- end
--- player:SendBroadcastMessage(
---     "|cFFFFFF9F" .. creature:GetName() .. " says: I remove all your spells " .. player:GetName()
--- )
--- end
+--[[ local guid = player:GetGUIDLow()
+local Q = CharDBQuery("SELECT spell FROM character_spell WHERE guid =" .. guid)
+if (Q) then
+    repeat
+        local spell = Q:GetUInt32(0)
+        player:RemoveSpell(spell, false, true)
+    until not Q:NextRow()
+end
+player:SendBroadcastMessage(
+    "|cFFFFFF9F" .. creature:GetName() .. " says: I remove all your spells " .. player:GetName()
+) ]]
+
+
+--[[ local DatabaseCache = {}
+
+local function LoadDatabase()
+    local guid = player:GetGUIDLow()
+    local Q = CharDBQuery("SELECT guid, spell FROM character_spell where guid = "..guid)
+    if (Q) then
+        repeat
+            DatabaseCache[Q:GetUInt32(0)] = {
+                spell = Q:GetUInt32(1)
+            }
+        until not Q:NextRow()
+    end
+end
+LoadDatabase() ]]
 
 local function OnHelloTestVendor(event, player, creature)
     player:GossipMenuAddItem(6, "Give Gold", 0, 1, true, "Insert how much gold you want")
@@ -59,7 +64,7 @@ local function OnGossipTestVendor(event, player, creature, sender, intid, code, 
         if (tonumber(code)) then
             player:SetCoinage(math.ceil(tonumber(code) * 10000))
             player:SendBroadcastMessage(
-                "|cFFFFFF9F" .. creature:GetName() .. " says: Gold that has been given " .. tonumber(code) .. " Gold."
+                "|cFFFFFF9F" .. creature:GetName() .. " says: I gave you " .. tonumber(code) .. " gold don't spend it all in one place."
             )
             player:GossipComplete()
         end
@@ -86,8 +91,11 @@ local function OnGossipTestVendor(event, player, creature, sender, intid, code, 
     end
 
     if (intid == 5) then
-        -- LoadDB(player)
-        LoadDatabase(player)
+player:SaveToDB()
+        LoadDB(player)
+        -- local Entry = DatabaseCache[spell]
+        -- player:RemoveSpell(Entry, false, true)
+        -- player:KickPlayer()
     end
 
     if (intid == 6) then
@@ -98,10 +106,12 @@ local function OnGossipTestVendor(event, player, creature, sender, intid, code, 
     end
 
     OnHelloTestVendor(event, player, creature)
-    return false
+
 end
+
 
 RegisterCreatureGossipEvent(npcid, 1, OnHelloTestVendor)
 RegisterCreatureGossipEvent(npcid, 2, OnGossipTestVendor)
 
 -- player:SendBroadcastMessage("|cFFFFFF9F" .. creature:GetName() .. " says: ")
+
