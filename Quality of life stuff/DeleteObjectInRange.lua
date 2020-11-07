3a190ee3c1ff906af899a7f1a4bbce425ae7e66c
Clotic <isidorssona@gmail.com>
2020-11-07 15:32:49 +0100

    Updat eadded GUID

--- Quality of life stuff/DeleteObjectInRange.lua
+++ Quality of life stuff/DeleteObjectInRange.lua
@@ -3,7 +3,7 @@
 local MSG_OBD = "#obd" -- What Message in chat box
 local MSG_OBC = "#obc" -- What Message in chat box
 
-local RANGE = 10 -- What range you want the delete/check to detect in.
+local RANGE = 10 -- What range you want the delete to detect in.
 
 -------------------------------------------------------------------------
 local function DeleteObjectNearby(event, player, msg, _, lang)
@@ -14,25 +14,27 @@ local function DeleteObjectNearby(event, player, msg, _, lang)
         end
 
         if not player:GetNearestGameObject(RANGE) then
-            player:SendBroadcastMessage("There is nothing near. Try increase the range or move closer to a object.")
+            player:SendBroadcastMessage("There is nothing except the void.")
             return false
         end
         local a = player:GetNearestGameObject(RANGE)
         local b = a:GetEntry()
         local c = a:GetDisplayId()
         local d = a:GetName()
+local e = a:GetDBTableGUIDLow()
+        -- local e = tostring(GetObjectGUID(a:GetGUIDLow(), a:GetEntry()))
 
         if (msg:find(MSG_OBD)) then
             a:RemoveFromWorld(true)
 
             player:SendBroadcastMessage(
                 "|cFFffffff Name: |r " ..
-                    d .. " |cFFffffff Entry Deleted: |r  " .. b .. " |cFFffffff Display ID: |r " .. c .. ""
+                    d .. " |cFFffffff Entry Deleted:|r " .. b .. " |cFFffffff GUID: |r " ..e.. " |cFFffffff Display ID: |r " .. c .. ""
             )
             return false
         elseif (msg:find(MSG_OBC)) then
             player:SendBroadcastMessage(
-                "|cFFffffff Name: |r " .. d .. " |cFFffffff Entry:|r  " .. b .. " |cFFffffff Display ID: |r " .. c .. ""
+                "|cFFffffff Name: |r " .. d .. " |cFFffffff Entry:|r  " .. b .. " |cFFffffff GUID: |r " ..e.. " |cFFffffff Display ID: |r " .. c .. ""
             )
             return false
         end
