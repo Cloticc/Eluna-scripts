8061458b6a17095e8e16d84bf2c48db2f89ee70d
Clotic <isidorssona@gmail.com>
2022-03-17 11:09:14 +0100

    Remove a comment

--- Quality of life stuff/Commands/Goldfix SelectTarget.lua
+++ Quality of life stuff/Commands/Goldfix SelectTarget.lua
@@ -1,8 +1,9 @@
 local MSG_GOLD = "mod gold" --#command for triggering .mod gold 123 or -123 to remove gold
-local FILE_NAME = string.match(debug.getinfo(1,'S').source, "[^/\\]*.lua$")
+local FILE_NAME = string.match(debug.getinfo(1, "S").source, "[^/\\]*.lua$")
 local function goldFix(event, player, command)
-    -- local function goldFix(event, player, msg, Type, lang)
-        if not player:IsGM() then return end
+    if not player:IsGM() then
+        return
+    end
 
     if (command:find(MSG_GOLD)) then
         local selectTarget = player:GetSelection()
--- Quality of life stuff/Commands/Goldfix self.lua
+++ Quality of life stuff/Commands/Goldfix self.lua
@@ -1,12 +1,14 @@
 local MSG_GOLD = "mod gold" --#command for triggering .mod gold 123 or -123 to remove gold
-local FILE_NAME = string.match(debug.getinfo(1,'S').source, "[^/\\]*.lua$")
+local FILE_NAME = string.match(debug.getinfo(1, "S").source, "[^/\\]*.lua$")
 local function goldFix(event, player, command)
-        if not player:IsGM() then return end
+    if not player:IsGM() then
+        return
+    end
 
     if (command:find(MSG_GOLD)) then
         local copperAmt = tonumber(tostring(string.gsub(command, "mod gold ", "")))
-        player:ModifyMoney(copperAmt*10000)
-        player:SendBroadcastMessage("|cff00ff00[World]|r You have been given "..copperAmt.." gold.")
+        player:ModifyMoney(copperAmt * 10000)
+        player:SendBroadcastMessage("|cff00ff00[World]|r You have been given " .. copperAmt .. " gold.")
 
         return false
     end
