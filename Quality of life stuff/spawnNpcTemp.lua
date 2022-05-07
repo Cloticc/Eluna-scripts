a176b97205787f0b4436e5d09e34dc60f019dfe6
Clotic <isidorssona@gmail.com>
2022-05-07 10:25:02 +0200

    Update spawnNpcTemp.lua

--- Quality of life stuff/spawnNpcTemp.lua
+++ Quality of life stuff/spawnNpcTemp.lua
@@ -1,38 +1,38 @@
 
 --[[
-    spawnNpcTemp.lua
-    script to spawn a npc infront of player location with despawn time in minutes.
-    usage: .npt entry min
+Spawn a npc infront of you with a given time.
+Example: /npt 123 10 -- Spawns a npc with id 123 for 10 minutes.
+
 
  ]]
 
-local MSG_NPE = "npt" --#command for triggering
+local MSG_NPT = "npt" --#command for triggering
 
+local FILE_NAME = string.match(debug.getinfo(1, "S").source, "[^/\\]*.lua$")
 
 local function splitString(str, delimiter)
     local result = {}
-    local from = 1
+    local from = 1  -- start of the word
     local delim_from, delim_to = string.find(str, delimiter, from)
     while delim_from do
         table.insert(result, string.sub(str, from, delim_from - 1))
         from = delim_to + 1
-        delim_from, delim_to = string.find(str, delimiter, from)
+        delim_from, delim_to = string.find(str, delimiter, from)  -- find the next word
     end
-    table.insert(result, string.sub(str, from))
+    table.insert(result, string.sub(str, from))  -- insert tailing element
     return result
 end
 
-local FILE_NAME = string.match(debug.getinfo(1, "S").source, "[^/\\]*.lua$")
-local function spawnNPE(event, player, command)
+local function spawnNpt(event, player, command)
     if player:GetGMRank() <= 2 then
         player:SendBroadcastMessage("You Dont have access to this.")
         return false
     end
-    if (command:find(MSG_NPE)) then
--- set so i can set ID then timeDespawn
-        local args = splitString(command, " ")
-        local npcId = tonumber(args[2])
-        local time = tonumber(args[3])
+    if (command:find(MSG_NPT)) then
+
+        local args = splitString(command, " ")  --split the command into args
+        local npcId = tonumber(args[2]) --get the npcId from the args
+        local time = tonumber(args[3]) --get the time to spawn
 
 
         local angle = player:GetO()
@@ -47,4 +47,4 @@ local function spawnNPE(event, player, command)
     end
 end
 
-RegisterPlayerEvent(42, spawnNPE)
+RegisterPlayerEvent(42, spawnNpt)
