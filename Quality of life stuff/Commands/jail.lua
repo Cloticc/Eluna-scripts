3d79ecf972aee3ba851c084bc47c8700c3adaad7
Clotic <isidorssona@gmail.com>
2022-05-16 18:10:36 +0200

    Update jail.lua
    Fixed teleport issue cus i am dumb:d

--- Quality of life stuff/Commands/jail.lua
+++ Quality of life stuff/Commands/jail.lua
@@ -18,11 +18,11 @@ local PLAYER_EVENT_ON_UPDATE_ZONE = 27
 
 local jailSpell = 7
 
-local jailLoc = {1, 16206.40625, 16216.223632812, 2.5386452674866, 1.0740673542023} --gmisland
+local jailLoc = {1, 16206.40625, 16216.223632812, 2.5386452674866, 1.0740673542023} -- gmisland
 -- 1,  16219.421875,  16403.408203125,  -64.37858581543,  0.017713271081448
 
-local unJailAlliance = {0, -8865.5966796875, 673.04827880859, 100.587059021, 4.9373631477356} --stormwind
-local unJailHorde = {1, 1635.765625, -4443.3647460938, 16.921722412109, 2.5195200443268} --orgrimmar
+local unJailAlliance = {0, -8865.5966796875, 673.04827880859, 100.587059021, 4.9373631477356} -- stormwind
+local unJailHorde = {1, 1635.765625, -4443.3647460938, 16.921722412109, 2.5195200443268} -- orgrimmar
 
 local function splitString(str, delimiter)
     local result = {}
@@ -46,50 +46,46 @@ end
 
 local function jailName(event, player, command)
 
-    if (command:find(MSG_JAIL)) then
+    if command:find(MSG_JAIL) then
         if player:GetGMRank() <= 2 then
             player:SendBroadcastMessage("You Dont have access to this.")
             return false
         end
         local args = splitString(command, " ")
         local name = tostring(args[2])
-
-
+        local time = tonumber(args[3])
+        print(command .. " " .. name)
         local player = GetPlayerByName(name)
         if player then
             player:Teleport(jailLoc[1], jailLoc[2], jailLoc[3], jailLoc[4], jailLoc[5])
-            player:RemoveItem(6948, 1)
-            player:CastSpell(player, 26, true)
             player:LearnSpell(jailSpell)
-
 
         else
             player:SendBroadcastMessage("Player not found.")
         end
 
     end
-end
-
-local function unJailName(event, player, command)
-
-    if (command:find(MSG_UNJAIL)) then
+    if command:find(MSG_UNJAIL) then
         if player:GetGMRank() <= 2 then
             player:SendBroadcastMessage("You Dont have access to this.")
             return false
         end
+
         local args = splitString(command, " ")
         local name = tostring(args[2])
+        local time = tonumber(args[3])
 
+        print(command .. " " .. name)
         local player = GetPlayerByName(name)
         if player then
+            player:RemoveSpell(jailSpell)
             if player:GetTeam() == 0 then
                 player:Teleport(unJailAlliance[1], unJailAlliance[2], unJailAlliance[3], unJailAlliance[4],
                     unJailAlliance[5])
-            else
+            elseif player:GetTeam() == 1 then
+
                 player:Teleport(unJailHorde[1], unJailHorde[2], unJailHorde[3], unJailHorde[4], unJailHorde[5])
             end
-            player:AddItem(6948, 1)
-            player:RemoveSpell(jailSpell)
 
         else
             player:SendBroadcastMessage("Player not found.")
@@ -99,5 +95,4 @@ local function unJailName(event, player, command)
 end
 
 RegisterPlayerEvent(PLAYER_EVENT_ON_COMMAND, jailName)
-RegisterPlayerEvent(PLAYER_EVENT_ON_COMMAND, unJailName)
 RegisterPlayerEvent(PLAYER_EVENT_ON_UPDATE_ZONE, zoneChangeWhileJail)
