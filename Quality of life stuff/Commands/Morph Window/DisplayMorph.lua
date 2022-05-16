e827e1bf9540602493892b75b82e76b4f454b4a1
Clotic <isidorssona@gmail.com>
2022-05-16 13:17:26 +0200

    Move files. Added Morph
    Added Morph window.

---
+++ Quality of life stuff/Commands/Morph Window/DisplayMorph.lua
@@ -0,0 +1,58 @@
+--[[
+    Open with #morph
+ A menu that allows player to go threw morph ids and see what they look like.
+
+
+ ]]
+
+
+
+
+ local PLAYER_EVENT_ON_CHAT = 18
+-- local GOSSIP_EVENT_ON_HELLO = 1
+local GOSSIP_EVENT_ON_SELECT = 2
+local MENU_ID = 1
+
+local chatMsg = "#morph"
+local morphMenu = {}
+
+local function menuToggle(event, player, object)
+    player:GossipClearMenu()
+    player:GossipMenuAddItem(0, "Next", 0, 1)
+    player:GossipMenuAddItem(0, "Demorph", 0, 2)
+    player:GossipMenuAddItem(0, "Prev", 0, 3)
+    player:GossipMenuAddItem(0, player:GetDisplayId() .. " <-Display ID Click to print to chat", 0, 4)
+    player:GossipSendMenu(1, player, 1)
+end
+
+local function onSelect(event, player, object, sender, intid, code, menu_id)
+    if (intid == 1) then
+        if (player:GetDisplayId() == flase) then player:SetDisplayId(1) end
+        player:SetDisplayId(player:GetDisplayId() + 1)
+
+        menuToggle(event, player)
+    end
+
+    if (intid == 2) then
+        player:DeMorph()
+        player:GossipComplete()
+    end
+    if (intid == 3) then
+        player:SetDisplayId(player:GetDisplayId() - 1)
+        menuToggle(event, player)
+    end
+    if (intid == 4) then
+        player:SendBroadcastMessage("Display ID: " .. player:GetDisplayId())
+        menuToggle(event, player)
+    end
+end
+
+local function openFrameForMorph(event, player, msg, lang, type)
+    if (msg:find(chatMsg)) then
+        menuToggle(event, player)
+        return false
+    end
+end
+
+RegisterPlayerEvent(PLAYER_EVENT_ON_CHAT, openFrameForMorph)
+RegisterPlayerGossipEvent(MENU_ID, GOSSIP_EVENT_ON_SELECT, onSelect)
---
+++ Quality of life stuff/Commands/Morph Window/readme.md
@@ -0,0 +1,8 @@
+# Morph change window
+
+## Description of the morph change window
+To open the window do #morph. You can morph yourself before and get higher value. So you don't need to spam the Next to get higher value.
+
+
+
+![alt](https://i.ibb.co/cc70CJ8/image.png)
