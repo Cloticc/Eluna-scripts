a42251b98a8404b5eaf9f3f16f40052f3f2d7ad8
Clotic <isidorssona@gmail.com>
2022-08-23 09:29:53 +0200

    zoneBuff

---
+++ Quality of life stuff/areaZoneBuff.lua
@@ -0,0 +1,28 @@
+local zoneBuffs = {
+    --zoneId, buffId
+    [123] = {123}
+}
+
+local PLAYER_EVENT_ON_UPDATE_ZONE = 27
+
+local function zoneBuff(event, player, newZone, newArea)
+    if player:IsDead() then --check if dead
+        return
+    end
+
+    for zone, buffs in pairs(zoneBuffs) do
+        if newZone ~= zone then --not in zone remove buff
+            for _, buff in pairs(buffs) do
+                player:RemoveAura(buff)
+            end
+        else
+            for _, buff in pairs(buffs) do --if in zone then apply buffs
+                if not player:HasAura(buff) then
+                    player:AddAura(buff, player)
+                end
+            end
+        end
+    end
+end
+
+RegisterPlayerEvent(PLAYER_EVENT_ON_UPDATE_ZONE, zoneBuff)
