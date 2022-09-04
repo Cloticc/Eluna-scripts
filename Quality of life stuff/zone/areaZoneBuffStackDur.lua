031c10248fc69b448e724e278413188bfd45424a
Clotic <isidorssona@gmail.com>
2022-09-04 14:28:36 +0200

    Fix zonebuff issue

--- Quality of life stuff/zone/areaZoneBuffStackDur.lua
+++ Quality of life stuff/zone/areaZoneBuffStackDur.lua
@@ -12,39 +12,30 @@ local zoneBuffs = {
 local PLAYER_EVENT_ON_UPDATE_ZONE = 27
 
 local function zoneBuff(event, player, newZone, newArea)
+    local playerZoneId = player:GetZoneId()
     if player:IsDead() then
         return
     end
 
-    local playerZoneId = player:GetZoneId()
-    local zoneCheck = zoneBuffs[playerZoneId]
-
-    if newZone then
-        for tableZoneId, tableBuffs in pairs(zoneBuffs) do
-            if tableZoneId ~= zoneCheck then
-                for _, buff in pairs(tableBuffs) do
-                    player:RemoveAura(buff.buffId)
-                end
-            end
+    for zoneId, buffs in pairs(zoneBuffs) do
+        for _, buff in pairs(buffs) do
+            player:RemoveAura(buff.buffId)
         end
     end
+    if zoneBuffs[playerZoneId] then
+        for _, buff in pairs(zoneBuffs[newZone]) do
+            player:AddAura(buff.buffId, player)
 
-    if zoneCheck then
-        for _, buff in pairs(zoneCheck) do
-            if not player:HasAura(buff.buffId) then
-                player:AddAura(buff.buffId, player)
-                if buff.amount > 1 then
-                    local aura = player:GetAura(buff.buffId)
-                    if aura then
-                        aura:SetStackAmount(buff.amount)
-                        if buff.duration > 2 then
-                            aura:GetDuration(aura:SetDuration(buff.duration))
-                        end
+            if buff.amount > 1 then
+                local aura = player:GetAura(buff.buffId)
+                if aura then
+                    aura:SetStackAmount(buff.amount)
+                    if buff.duration > 2 then
+                        aura:GetDuration(aura:SetDuration(buff.duration))
                     end
                 end
             end
         end
     end
 end
-
 RegisterPlayerEvent(PLAYER_EVENT_ON_UPDATE_ZONE, zoneBuff)
