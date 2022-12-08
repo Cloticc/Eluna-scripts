176b48699fe55cad3919dc5b70722a9cc45f3cd5
Clotic <isidorssona@gmail.com>
2022-12-08 14:11:55 +0100

    Update areaZoneBuffer.lua
    Fixed so they don't get spammed when you are in correct zone already.

--- Quality of life stuff/zone/areaZoneBuffer.lua
+++ Quality of life stuff/zone/areaZoneBuffer.lua
@@ -1,71 +1,92 @@
-local min1 = 60000
-local min5 = 300000
-local min10 = 600000
-local min30 = 1800000
-local min60 = 3600000
-
-
---[[
-
-the zoneBuffs how to use it: the [1] = zoneID, buffId is the spellId u want to applie, amount is if it stacks might work on non stackable buffs aswell, duration is the duration of the buff in seconds. Added example for the buff 1 hour in table can either use variable or just put the number in there.
-
- ]]
-
-
+-- {zoneId, buffId, amount, duration},
+--duration is in miliseconds
+--amount not all spells have stacks
 -- example: Below is an example of a zone buff that gives battleShout in Elwynn Forest and in Westfall Commanding Shout. duration is in miliseconds.
 local zoneBuffs = {
-
+    --[[
+        [zoneId] = { ---this is the format
+            { buffId = 80893, amount = 0, duration = 0 },
+            { buffId = 80896, amount = 5, duration = 0 }
+        }
+    ]]
     [1] = {
-        { buffId = 47440, amount = 5, duration = min60 }, --atm it's commanding shout for 1 hour
-        { buffId = 47436, amount = 5, duration = 3600000 } --atm it's battle shout for 1 hour
+        { buffId = 80893, amount = 5, duration = 0 },
+        { buffId = 80896, amount = 5, duration = 0 }
     },
-    [12] = {
-        { buffId = 47440, amount = 5, duration = 0 }, --atm it's commanding shout
-        { buffId = 47436, amount = 5, duration = 0 } --atm it's battle shout
+    [12] = { --Elwynn Forest
+        { buffId = 80893, amount = 2, duration = 0 },
+        { buffId = 80896, amount = 5, duration = 0 }
+    },
+    [40] = { --Westfall
+        { buffId = 80893, amount = 25, duration = 0 },
+        { buffId = 80896, amount = 25, duration = 0 }
+    },
+    [10] = { --Duskwood
+        { buffId = 47241, amount = 0, duration = 1800000 },
+        { buffId = 47893, amount = 0, duration = 0 },
+        { buffId = 34074, amount = 0, duration = 0 },
+        { buffId = 80896, amount = 3, duration = 0 }
+    },
+    [141] = {
 
+        { buffId = 80893, amount = 5, duration = 0 },
+        { buffId = 80896, amount = 5, duration = 0 }
+    },
+    [14] = {
+        { buffId = 80893, amount = 5, duration = 0 },
+        { buffId = 80896, amount = 5, duration = 0 }
     }
 }
-local PLAYER_EVENT_ON_UPDATE_ZONE = 27
 
-local function checkZone(_, player, _, _)
+
+local PLAYER_EVENT_ON_UPDATE_ZONE = 27
+local playerZone = {}
+
+local function checkZone(event, player, _, _)
     local zoneId = player:GetZoneId() -- get the zone id
     local zone = zoneBuffs[zoneId] -- get the zone from the table
+    local currentZone = playerZone[player:GetGUIDLow()] -- get the current zone from the playerZone table
 
-    for _, buff in pairs(zoneBuffs) do
-        for _, buffId in pairs(buff) do
-            local aura = player:GetAura(buffId.buffId)
-            if aura then
-                aura:Remove()
+    if zone ~= currentZone then
+        for _, zoneBuff in pairs(zoneBuffs) do
+            for _, buffId in pairs(zoneBuff) do
+                local aura = player:GetAura(buffId.buffId)
+                if aura then
+                    aura:Remove()
+                end
             end
         end
+        playerZone[player:GetGUIDLow()] = zone
     end
 
 
-    if zone then
 
+    if zone ~= currentZone then
+        if not zone then
+            return
+        end
         for _, buff in ipairs(zone) do
+            if not player:HasAura(buff.buffId) then
 
-            if player:HasAura(buff.buffId) then
-                player:RemoveAura(buff.buffId)
-            end
+                player:AddAura(buff.buffId, player)
+                if buff.amount > 1 then
+                    local aura = player:GetAura(buff.buffId)
 
-            player:AddAura(buff.buffId, player)
-            -- player:CastSpell(player, buff.buffId, true) --Bad
+                    if aura then
+                        aura:SetStackAmount(buff.amount)
+                    end
+                end
+                if buff.duration > 1 then
+                    local aura = player:GetAura(buff.buffId)
 
-            if buff.amount > 1 then
-
-                local aura = player:GetAura(buff.buffId)
-                if aura then aura:SetStackAmount(buff.amount) end
-
-            end
-            if buff.duration > 1 then
-
-                local aura = player:GetAura(buff.buffId)
-                if aura then aura:SetDuration(buff.duration) end
-
+                    if aura then
+                        aura:SetDuration(buff.duration)
+                    end
+                end
             end
         end
         return
+
     end
 end
 
