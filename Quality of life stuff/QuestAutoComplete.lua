96e82ed0d735e2abbd83e4e612a94b103d300f7f
Clotic <isidorssona@gmail.com>
2022-12-22 17:09:36 +0100

    Update QuestAutoComplete.lua
    refactor change so it select the player not load all

--- Quality of life stuff/QuestAutoComplete.lua
+++ Quality of life stuff/QuestAutoComplete.lua
@@ -1,34 +1,80 @@
---[[ Never really use sql querys so might be a smarter way of doing this. You live and you learn ]]
+--[[ Refactor the script 2022-12-22 ]]
 
-local MSG_QC = "#qc" -- Message to complete all the quest in log.
+local MSGQC = "#qc" -- Command
+local RANK_REQUIRED = 3 -- GM Rank
+local GOLD_DEDUCTION_PERCENTAGE = 0.025 -- Set the amount of gold you want to take from the player (0.025 = 2.5%)
 
-local function UpdateQuest(player)
-    local query = WorldDBQuery("SELECT ID, LogTitle FROM quest_template")
-    if query then
+local QUEST_STATUS_COMPLETE = 1 -- 1 = complete
+local QUEST_STATUS_INCOMPLETE = 3 -- 3 = incomplete
+
+local PLAYER_EVENT_ON_CHAT = 18 -- (event, player, msg, Type, lang) - Can return false, newMessage
+
+local function completeQuestsForPlayer(player)
+    local playerName = player:GetName()
+    local playerGold = player:GetCoinage()
+    local goldToDeduct = math.floor(playerGold * GOLD_DEDUCTION_PERCENTAGE)
+
+    local incompleteQuests = {}
+
+    -- Find all incomplete quests for the player
+    local questQuery =
+    WorldDBQuery(
+        "SELECT ID, LogTitle FROM  world.quest_template WHERE ID IN (SELECT quest FROM characters.character_queststatus WHERE guid = "
+        ..
+        player:GetGUIDLow() .. " AND status = " .. QUEST_STATUS_INCOMPLETE .. ")"
+    )
+    if questQuery then
         repeat
-            local row = query:GetRow()
-            local row2 = query:GetRow()
-            local Quest = tonumber(row["ID"])
-            local LogTitle = string.format(row2["LogTitle"])
-            if player:HasQuest(Quest) then
-                player:CompleteQuest(Quest)
-                player:SendBroadcastMessage("|cFFffffffLogTitle:|r " .. LogTitle .. " |cFFffffffID:|r " .. Quest .. "")
-            end
-        until not query:NextRow()
+            local questRow = questQuery:GetRow()
+            local questId = tonumber(questRow["ID"])
+            local questTitle = questRow["LogTitle"]
+
+            table.insert(incompleteQuests, { id = questId, title = questTitle })
+        until not questQuery:NextRow()
+    end
+
+    if #incompleteQuests == 0 then
+        player:SendBroadcastMessage(
+            "|cFFffffff|cFF00ff00The World |r|cFFffffff You have no incomplete quests. |cFF00ff00"
+        )
+        return
+    end
+
+    -- Complete quests and deduce gold for each incomplete quest
+    for _, quest in pairs(incompleteQuests) do
+        player:CompleteQuest(quest.id)
+        player:ModifyMoney(-goldToDeduct)
+
+        player:SendBroadcastMessage(
+            string.format(
+                "|cFFffffff|cFF00ff00Quest: |r|cFFffffff%s|cFFffffff|cFF00ff00 ID:|r |cFFffffff%d|cFFffffff|cFFffffff completed for you"
+                ,
+                quest.title,
+                quest.id
+            )
+        )
+        player:SendBroadcastMessage(
+            string.format(
+                "|cFFffffff|cFF00ff00The World |r|cFFffffff has taken |cFFffffff|cFF00ff00%.2f%%|r|cFFffffff of the |cFF00ff00%d|r|cFFffffff gold you had"
+                ,
+                GOLD_DEDUCTION_PERCENTAGE * 100,
+                goldToDeduct
+            )
+        )
     end
 end
 
-local function QuestAutoComplete(event, player, msg, Type, lang)
-    local gmRank = player:GetGMRank()
-    if (gmRank >= 3) then
-        local msg = msg:lower()
-        if (msg == MSG_QC) then
-            UpdateQuest(player)
-            player:SendBroadcastMessage(" Quest Been Completed  ")
-            return false
+local function onChatMessage(event, player, message, _, _)
+    if message:find(MSGQC) then
+        -- Check if the player has the required GM rank
+        if player:GetGMRank() < RANK_REQUIRED then
+            player:SendBroadcastMessage("|cFFffffff|cFF00ff00The World |r|cFFffffff Don't have access. |cFF00ff00")
+            return
         end
+        player:SaveToDB()
+        completeQuestsForPlayer(player)
+        return false
     end
 end
 
-RegisterPlayerEvent(18, QuestAutoComplete)
-
+RegisterPlayerEvent(PLAYER_EVENT_ON_CHAT, onChatMessage)
